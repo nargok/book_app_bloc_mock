ce61c9603f39de805cdd4bdca8d1c3e732c98c20
nargok <nargok@gmail.com>
2020-04-26 13:16:02 +0900

    Update book_list.dart
    Expanded Listview

--- lib/ui/book_list.dart
+++ lib/ui/book_list.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import '../models/book.dart';
 
 Widget buildList(AsyncSnapshot<BookModel> snapshot) {
-  return ListView.builder(
-    itemCount: (snapshot.hasData) ? snapshot.data.items.length : 0,
-    shrinkWrap: true,
-    itemBuilder: (context, index) {
-      return ListTile(
-        title: Text('${snapshot.data.items[index].title}'),
-      );
-    },
+  return Expanded( // Widgetの中を固定の比率で表示する
+    child: ListView.builder(
+      itemCount: (snapshot.hasData) ? snapshot.data.items.length : 0,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('${snapshot.data.items[index].title}'),
+        );
+      },
+    ),
   );
 }
